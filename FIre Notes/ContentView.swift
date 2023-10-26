a934c0b280d387540742d9c30d36cb5f0d0e8bda
Samuel Martins <samuel.ubuntu@gmail.com>
2023-10-26 11:04:56 -0300

    Initial Commit

---
+++ FIre Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FIre Notes
+//
+//  Created by Samuel Martins on 26/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FIre Notes/FIre_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIre_NotesApp.swift
+//  FIre Notes
+//
+//  Created by Samuel Martins on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FIre_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
